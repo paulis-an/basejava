3ffbfc0bf8ff5a5c7a68fc1bfe8d8921b730c828
Pavel An <anisimov@cdkino.ru>
2021-04-20 12:41:33 +0300

    fixed Lesson06 20-04

--- test/com/project_basejava/webapp/storage/AbstractStorageTest.java
+++ test/com/project_basejava/webapp/storage/AbstractStorageTest.java
@@ -8,9 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public abstract class AbstractStorageTest {
